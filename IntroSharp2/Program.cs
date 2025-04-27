08a8c7cd5bc2bbe7689299cc5709c0737cda3c89
Mark Maichenko <marcusmay0202@gmail.com>
2025-04-27 18:22:57 +0300

    task2

--- IntroSharp2/Program.cs
+++ IntroSharp2/Program.cs
@@ -8,7 +8,26 @@ public class Programa
     public static void Main(string[] args)
     {
         
-        
+        Console.WriteLine("Input array size");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[] array = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(1, 100);
+        }
+        Console.WriteLine("Input number");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int count = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (array[i] < number)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("Array: " + string.Join(", ", array));
+        Console.WriteLine("Count of numbers less than " + number + ": " + count);
         
     }
 }
--- IntroSharp2/obj/Debug/net8.0/IntroSharp2.AssemblyInfo.cs
+++ IntroSharp2/obj/Debug/net8.0/IntroSharp2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IntroSharp2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d98fd42379d17ac8a2c752ca424fbc072acd01")]
 [assembly: System.Reflection.AssemblyProductAttribute("IntroSharp2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IntroSharp2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
