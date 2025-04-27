77b1370060b011e94f558b92502357a9554818a2
Mark Maichenko <marcusmay0202@gmail.com>
2025-04-27 18:51:57 +0300

    task9

--- IntroSharp2/Program.cs
+++ IntroSharp2/Program.cs
@@ -1,5 +1,6 @@
 ﻿/*/
-
+Створіть додаток для підрахунку кількості входжень підрядка в рядок. 
+Користувач вводить вихідний рядок і слово для пошуку. Додаток відображає результат пошуку.
 /*/
 
 public class Programa
@@ -7,7 +8,18 @@ public class Programa
     public static void Main(string[] args)
     {
         
-        
+        Console.WriteLine("Write the string:");
+        string str = Console.ReadLine();
+        Console.WriteLine("Write the word to find:");
+        string word = Console.ReadLine();
+        int count = 0;
+        int index = str.IndexOf(word);
+        while (index != -1)
+        {
+            count++;
+            index = str.IndexOf(word, index + word.Length);
+        }
+        Console.WriteLine($"The word '{word}' appears {count} times in the string.");
         
     }
 }
