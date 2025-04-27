8132bb2b9bea61e9db60d3aac5c168dbea58ea06
Mark Maichenko <marcusmay0202@gmail.com>
2025-04-27 18:39:44 +0300

    task6

--- IntroSharp2/Program.cs
+++ IntroSharp2/Program.cs
@@ -1,5 +1,5 @@
 ﻿/*/
-
+Користувач вводить речення з клавіатури. Вам необхідно підрахувати кількість слів у ньому.
 /*/
 
 public class Programa
@@ -7,7 +7,30 @@ public class Programa
     public static void Main(string[] args)
     {
         
-        
+        Console.WriteLine("Write a sentence:");
+        string sentence = Console.ReadLine();
+        int Words = 0;
+        bool space = false;
+        foreach (char c in sentence)
+        {
+            if (char.IsWhiteSpace(c))
+            {
+                if (space)
+                {
+                    Words++;
+                    space = false;
+                }
+            }
+            else
+            {
+                space = true;
+            }
+        }
+        if (space)
+        {
+            Words++;
+        }
+        Console.WriteLine($"The number of words in the sentence is: {Words}");
         
     }
 }
