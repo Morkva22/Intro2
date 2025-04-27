c2e0d1f9e07ed5b6e8e3f0788b12666e9f0dff0d
Mark Maichenko <marcusmay0202@gmail.com>
2025-04-27 18:44:30 +0300

    task7

--- IntroSharp2/Program.cs
+++ IntroSharp2/Program.cs
@@ -1,13 +1,25 @@
 ﻿/*/
-
+Користувач вводить речення з клавіатури.
+ Вам необхідно перевернути кожне слово речення і відобразити результат на екран.
 /*/
 
 public class Programa
 {
     public static void Main(string[] args)
     {
-        
-        
-        
+
+        Console.WriteLine("Input a sentence:");
+        string sentence = Console.ReadLine();
+        string[] words = sentence.Split(' ');
+        string reversedSentence = "";
+        foreach (string word in words)
+        {
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            reversedSentence += new string(charArray) + " ";
+        }
+
+        Console.WriteLine("Reversed sentence:");
+        Console.WriteLine(reversedSentence.Trim());
     }
 }
