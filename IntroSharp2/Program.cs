58f3c90b144ba6de515669ea6a796051cdec2723
Mark Maichenko <marcusmay0202@gmail.com>
2025-05-07 14:39:09 +0300

    task5hm done..

--- IntroSharp2/Program.cs
+++ IntroSharp2/Program.cs
@@ -1,13 +1,60 @@
 ﻿/*/
-
+Користувач з клавіатури вводить у рядок арифметичний вираз.
+Додаток має порахувати його результат. Необхідно підтримувати тільки дві операції: + і -
 /*/
+
+using System;
 
 public class Programa
 {
     public static void Main(string[] args)
     {
-        
-        
-        
+        Console.WriteLine("Input arithmetic expression (only + and -):");
+        string input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Input cannot be empty.");
+            return;
+        }
+
+        try
+        {
+            int result = EvaluateExpression(input);
+            Console.WriteLine("Result: " + result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+
+    public static int EvaluateExpression(string expression)
+    {
+        string[] tokens = expression.Split(new char[] { '+', '-' }, StringSplitOptions.RemoveEmptyEntries);
+        int result = 0;
+        int index = 0;
+
+        for (int i = 0; i < expression.Length; i++)
+        {
+            if (char.IsDigit(expression[i]))
+            {
+                if (index < tokens.Length)
+                {
+                    int number = int.Parse(tokens[index]);
+                    if (i == 0 || expression[i - 1] == '+')
+                    {
+                        result += number;
+                    }
+                    else if (expression[i - 1] == '-')
+                    {
+                        result -= number;
+                    }
+                    index++;
+                }
+            }
+        }
+
+        return result;
     }
 }
